bf9d7f25180f8221e714596b1040f1eb9f9dade6
Sławomir Śledź <slawomir.sledz@gmail.com>
2021-06-17 22:51:54 +0200

    solved homework4

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -7,16 +7,16 @@
 
 module Week04.Homework where
 
-import Data.Aeson                 (FromJSON, ToJSON)
-import Data.Functor               (void)
-import Data.Text                  (Text, unpack)
-import GHC.Generics               (Generic)
-import Ledger
-import Ledger.Ada                 as Ada
-import Ledger.Constraints         as Constraints
-import Plutus.Contract            as Contract
-import Plutus.Trace.Emulator      as Emulator
-import Wallet.Emulator.Wallet
+import           Data.Aeson             (FromJSON, ToJSON)
+import           Data.Functor           (void)
+import           Data.Text              (Text, unpack)
+import           GHC.Generics           (Generic)
+import           Ledger
+import           Ledger.Ada             as Ada
+import           Ledger.Constraints     as Constraints
+import           Plutus.Contract        as Contract
+import           Plutus.Trace.Emulator  as Emulator
+import           Wallet.Emulator.Wallet
 
 data PayParams = PayParams
     { ppRecipient :: PubKeyHash
@@ -29,14 +29,23 @@ payContract :: Contract () PaySchema Text ()
 payContract = do
     pp <- endpoint @"pay"
     let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    void $ submitTx tx
+    Contract.handleError (\err -> Contract.logError $ "Caught error: " ++ unpack err) (void $ submitTx tx)
     payContract
 
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace x y = undefined -- IMPLEMENT ME!
+payTrace x y = do
+    h1 <- activateContractWallet (Wallet 1) payContract
+    payWallet2 h1 x
+    void $ waitUntilSlot 1
+    payWallet2 h1 y
+    void $ waitUntilSlot 1
+    where
+        payWallet2 :: ContractHandle () PaySchema Text -> Integer -> EmulatorTrace ()
+        payWallet2 h n = callEndpoint @"pay" h $ PayParams (pubKeyHash $ walletPubKey $ Wallet 2) n
+
 
 payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 1000000 2000000
